23e2cb9e8a4049d0990eee0489a0c8c2797663d5
Aleksey Hariton <aleksey_hariton1@epam.com>
2017-01-11 18:00:13 +0300

    GPG config fix #2

--- recipes/system_install.rb
+++ recipes/system_install.rb
@@ -36,7 +36,7 @@ home_dir = node['rvm']['gpg']['homedir']
 execute 'Adding gpg key' do
   command "`which gpg2 || which gpg` --keyserver #{key_server} --homedir #{home_dir} --recv-keys #{node['rvm']['gpg']['key']}"
   only_if 'which gpg2 || which gpg'
-  not_if { node['rvm']['gpg_key'].empty? }
+  not_if { node['rvm']['gpg']['key'].empty? }
 end
 
 rvm_installation("root")
